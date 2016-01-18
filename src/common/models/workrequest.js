0fc7142a2a7f2fdf1ac80735fad8280c809f0a43
vamsee <vakkiraju@chegg.com>
2016-01-18 10:12:48 -0800

    work request changes

--- src/common/models/workrequest.js
+++ src/common/models/workrequest.js
@@ -1,15 +1,25 @@
 module.exports = function (WorkRequest) {
 
+    //This is the main code that deals with submitting a request by SS
     WorkRequest.submit =
         function (workrequest, cb) {
             WorkRequest.create(workrequest,
                 function (errCreate, WorkRequestObj) {
+                    if (errCreate) {
+                        console.log(errCreate);
+                        return;
+                    }
                     WorkRequest.findById(WorkRequestObj.id,
                         function (errFind, WorkRequestFoundObj) {
+                            if (errFind) {
+                                console.log(errFind);
+                                return;
+                            }
                             cb(null, WorkRequestFoundObj.id);
                         });
                 });
         };
+
 
     WorkRequest.initiateSearch = function (WorkRequest) {
         //
