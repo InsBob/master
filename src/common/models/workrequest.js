41b98a0238de683b4d469eaa3988d161288e79f3
vamsee <vakkiraju@chegg.com>
2016-01-22 20:19:32 -0800

    Add more logic towards SP match

--- src/common/models/workrequest.js
+++ src/common/models/workrequest.js
@@ -1,30 +1,39 @@
+var match = require("../../server/match/SPMatch").getInstance();
+var Workflow = require("../models/workflow");
+
 module.exports = function (WorkRequest) {
+    var DEFAULT_MATCH_COUNT = 5;
+
 
     //This is the main code that deals with submitting a request by SS
     WorkRequest.submit =
         function (workrequest, cb) {
             WorkRequest.create(workrequest,
-                function (errCreate, WorkRequestObj) {
+                function (errCreate, workRequestObj) {
                     if (errCreate) {
                         console.log(errCreate);
                         return;
                     }
-                    WorkRequest.findById(WorkRequestObj.id,
-                        function (errFind, WorkRequestFoundObj) {
-                            if (errFind) {
-                                console.log(errFind);
-                                return;
-                            }
-                            cb(null, WorkRequestFoundObj.id);
-                        });
+
+                    var ssId = workRequestObj.SSid;
+                    //Get matched ids of SPs
+                    var spList = match.getMatchedSPIds(workRequestObj, DEFAULT_MATCH_COUNT);
+
+                    //Create Workflow with each of these SPs and the current SS
+                    //For each of the workflow items, create a notification push request to the phones
+                    for (var i = 0; i < spList.length; i++) {
+                        var spId = spList[i];
+                        var workflow = new Workflow(spId, ssId);
+                        workflow.notifySPOnCreate();
+                    }
+
+                    //A batch job runs every minute and based on a wait-time mapping, creates more workflows .
+                    // after 2 mins, 5 mins, 10 mins, 20 mins, 40 mins, 1 hour, 2 hr, 6 hr, 12 hr, 1 day, 1.5 days days till noOfRequests are met
+                    //After 2 days, if there is no response from any SPs, close all the workflows and the current request
+
+                    //Another batch job expires all old workflows.
                 });
         };
-
-
-    WorkRequest.initiateSearch = function (WorkRequest) {
-        //
-
-    };
 
     WorkRequest.remoteMethod('submit', {
         accepts: [
--- src/server/match/SPMatch.js
+++ src/server/match/SPMatch.js
@@ -1,10 +1,30 @@
 var util = require("util");
+var uuid = require('node-uuid');
 
-module.exports = function(SPMatch){
+var Match = (function () {
+    var instance;
+    function createInstance() {
+        var object = new Object("I am the instance");
+        object.getMatchedSPIds = function (workrequest, noOfSPNeeded)
+        {
+            var spList = [];
+            for(var x = 0; x <noOfSPNeeded; x++)
+            {
+                spList.add(uuid.v4());
+            }
+            return spList;
+        };
+        return object;
+    }
 
-  function getSPMatches(ssworkflow)
-  {
-    return [];
-  }
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
 
-};
+module.exports = Match;
