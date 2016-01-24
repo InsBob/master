8d739e2c83830add540f39ba03c7000c40652eac
vamsee <vakkiraju@chegg.com>
2016-01-24 01:41:00 -0800

    the new data models

--- src/server/boot/drop-and-recreate-all-tables.js
+++ src/server/boot/drop-and-recreate-all-tables.js
@@ -4,9 +4,9 @@ var dataSource = app.dataSources.pg;
 
 //Uncomment this to drop and recreate all db tables. use with caution
 //Pass app.modles.<ModelName> as first argument to only delete and add Modelname related tables. Without this argument, it will affect all tables.
-dataSource.automigrate(function (err) {
-    if (err) {
-        console.log(err);
-    }
-});
+//dataSource.automigrate(function (err) {
+//    if (err) {
+//        console.log(err);
+//    }
+//});
 
